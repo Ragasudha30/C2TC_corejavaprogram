5f173d07e51de8ec27c2a2b1f111c179255d83be
HP <HP@Sudha.bbrouter>
2025-07-21 21:43:28 +0530

    Basic Java Programs

---
+++ C2TC_corejavaprogram/src/Day1/Basics/CharDemo.java
@@ -0,0 +1,28 @@
+package Day1.Basics;
+
+public class CharDemo {
+
+	public static void main(String[] args) {
+			// assigning single character literal
+			char ch = 'a';
+			System.out.println("ch= "+ch);
+
+			// assigning number to char
+			char ch1 = 65;
+			System.out.println("ch1= "+ch1);
+
+			// assigning unicode to char
+			char var1 = '\u00A7';
+			System.out.println("var1= "+var1);
+
+			// Unicode representation
+			char var2 = '\u20AC';
+			System.out.println("var2= "+var2);
+
+			// ASCII code representation
+			int a = 'A';
+			System.out.println("a= "+a);
+
+	}
+
+}
