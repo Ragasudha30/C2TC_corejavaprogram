ef4215ff9fd973bbc9ad5251f7f1a3770b1bb215
HP <HP@Sudha>
2025-07-25 16:11:37 +0530

    Static and Final

---
+++ C2TC_corejavaprogram/src/Day_six/usingfinal/FinalMethodClassTest.java
@@ -0,0 +1,15 @@
+package Day_six.usingfinal;
+
+public class FinalMethodClassTest extends FinalMethodClass{
+
+	public static void main(String[] args) {
+		 // Compile time error because we cannot override the final method 
+		/*
+		 * @Override void show() {
+		 * 
+		 * System.out.println("This is the final method of FinalMethodEx class"); }
+		 */
+
+	}
+
+}
